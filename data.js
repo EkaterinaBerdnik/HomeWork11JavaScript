b09bd725c3787691d1f1e3eae7ce572a77d2606d
Ekaterina Berdnik <ek.berdnik@yandex.ru>
2023-03-13 16:10:04 +0300

    дз к семинару 11

---
+++ data.js
@@ -0,0 +1,38 @@
+const dataProducts = `[
+    {
+        "img": "img/men.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring   Australian, arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+    {
+        "img": "img/image.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian, arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+    {
+        "img": "img/gay.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+    {
+        "img": "img/men2.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+    {
+        "img": "img/women2.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+    {
+        "img": "img/women3.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    }
+]`
